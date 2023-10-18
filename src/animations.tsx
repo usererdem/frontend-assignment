546331790db2bc42a3f36f95748237f5811ad08f
erdemelvan <erdemelvan35@gmail.com>
2023-10-18 13:05:26 +0300

    feat: batchScan animation structured

--- src/animations.tsx
+++ src/animations.tsx
@@ -1,27 +1,3 @@
-export const phoneAnimation = {
-  hidden: {
-    y: 300,
-    opacity: 1,
-    transition: {
-      duration: 0,
-    },
-  },
-  show: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.7,
-      ease: "easeInOut",
-    },
-  },
-  exit: {
-    opacity: 0,
-    transition: {
-      duration: 0.3,
-    },
-  },
-};
-
 export const contentAnimation = {
   hidden: {
     opacity: 0,
@@ -59,12 +35,32 @@ export const borderAnimation = {
   },
 };
 
-export const batchScanContainerAnimation = {
+export const phoneAnimation = {
   hidden: {
+    y: 300,
+    opacity: 1,
     transition: {
-      staggerChildren: 0.4,
+      duration: 0,
     },
   },
+  show: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.7,
+      ease: "easeInOut",
+      staggerChildren: 0.7,
+    },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
+
+export const batchScanContainerAnimation = {
   show: {
     transition: {
       staggerChildren: 0.4,
@@ -72,15 +68,49 @@ export const batchScanContainerAnimation = {
   },
 };
 
-export const batchScanAnimation = {
+export const batchScanAnimation  = {
   hidden: {
     y: 300,
     transition: {
       duration: 0,
+      when: "afterChildren",
     },
   },
   show: {
+    y: 20,
+    scale: 0.6,
+    transition: {
+      duration: 0.4,
+    },
+  },
+};
+export const batchScanAnimation2  = {
+  hidden: {
     y: 0,
+    transition: {
+      duration: 0,
+      when: "afterChildren",
+    },
+  },
+  show: {
+    y: -210,
+    scale: 0.7,
+    transition: {
+      duration: 0.6,
+    },
+  },
+};
+export const batchScanAnimation3  = {
+  hidden: {
+    y: 0,
+    transition: {
+      duration: 0,
+      when: "afterChildren",
+    },
+  },
+  show: {
+    y: -440,
+    scale: 0.8,
     transition: {
       duration: 0.6,
     },
