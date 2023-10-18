bdd8ee5d5ac05cf040a416e0f2fb59b7fc2fc6aa
erdemelvan <erdemelvan35@gmail.com>
2023-10-18 19:17:07 +0300

    feat: export and share, sign and stamp animations added

--- src/components/PhoneFrame.tsx
+++ src/components/PhoneFrame.tsx
@@ -8,6 +8,12 @@ import {
   batchScanAnimation3,
   batchScanContainerAnimation,
   phoneAnimation,
+  ArrowIconAnimation,
+  exportShareAnimation1,
+  exportShareAnimation2,
+  exportShareAnimation3,
+  signStampAnimation1,
+  signStampAnimation2,
 } from "../animations";
 import useIcons from "./Icons/use-icons";
 
@@ -22,6 +28,10 @@ const PhoneFrame: React.FC<PhoneFrameProps> = (props) => {
     BrightnessSliderIconMobile,
     ContrastSliderIcon,
     ContrastSliderIconMobile,
+    PdfDocumentIcon,
+    TxtDocumentIcon,
+    JpgDocumentIcon,
+    ArrowIcon,
   } = useIcons();
   const controls = useAnimationControls();
 
@@ -45,6 +55,33 @@ const PhoneFrame: React.FC<PhoneFrameProps> = (props) => {
         className='relative flex justify-center flex-shrink-0 h-full'>
         <img className='h-full' src={props.imageUrl} alt='phone-image' />
 
+        {/* SIGN AND STAMP */}
+        {props.id === 2 && (
+          <>
+            <motion.div>
+              <motion.div
+                variants={signStampAnimation1}
+                className=' -left-16 top-1/2 absolute md:-left-24'>
+                {window.innerWidth <= 768 ? (
+                  <img src='src/assets/images/SignStampiconMobile.png'></img>
+                ) : (
+                  <img src='src/assets/images/SignStampicon.png'></img>
+                )}
+              </motion.div>
+              <motion.div
+                variants={signStampAnimation2}
+                className='absolute -right-16 top-1/3 md:-right-[92px]'>
+                {window.innerWidth <= 768 ? (
+                  <img src='src/assets/images/SignStampiconMobile2.png'></img>
+                ) : (
+                  <img src='src/assets/images/SignStampicon2.png'></img>
+                )}
+              </motion.div>
+            </motion.div>
+          </>
+        )}
+        
+        {/* BATCH SCAN */}
         {props.id === 3 && (
           <>
             <motion.div
@@ -65,6 +102,8 @@ const PhoneFrame: React.FC<PhoneFrameProps> = (props) => {
             </motion.div>
           </>
         )}
+
+        {/* ADVANCED FILTER */}
         {props.id === 4 && (
           <>
             <motion.div
@@ -95,17 +134,38 @@ const PhoneFrame: React.FC<PhoneFrameProps> = (props) => {
           </>
         )}
 
-        {/* (
-          switch(props.id){
-            case 1:
-              return (<></>)
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            default:
-          }
-         ) */}
+        {/* EXPORT AND SHARE */}
+        {props.id === 5 && (
+          <>
+            <motion.div
+              id='export-and-share-container'
+              className='overflow-hidden flex'
+              variants={batchScanContainerAnimation}
+              initial='hidden'
+              animate='show'>
+              <motion.div
+                className='absolute -left-6 bottom-2 w-[14px] md:w-5'
+                variants={ArrowIconAnimation}>
+                <ArrowIcon />
+              </motion.div>
+              <motion.div
+                className='absolute -left-8 bottom-0 w-[104px] md:w-40'
+                variants={exportShareAnimation1}>
+                <PdfDocumentIcon />
+              </motion.div>
+              <motion.div
+                className='absolute left-[60%] md:left-[50%] bottom-0 md:bottom-2  w-[82px] md:w-32'
+                variants={exportShareAnimation2}>
+                <JpgDocumentIcon />
+              </motion.div>
+              <motion.div
+                className='absolute -right-8 bottom-0 w-[94px] md:w-[9.5rem]'
+                variants={exportShareAnimation3}>
+                <TxtDocumentIcon />
+              </motion.div>
+            </motion.div>
+          </>
+        )}
       </motion.div>
     </div>
   );
